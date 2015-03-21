9c801a74be68f540e2482094b596a7879d9b09d1
Edward Byne <ejbyne@gmail.com>
2015-03-21 15:36:44 +0000

    further amends to css/html formatting

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ActiveRecord::Base
   belongs_to :user
   validates :image_file_name, presence: true
   validates :user_id, presence: true
-  has_attached_file :image, :styles => { :medium => "1000x1000>", :thumb => "200x200>" },
+  has_attached_file :image, :styles => { :medium => "1000x1000>", :thumb => "300x300>" },
                     :default_url => ":style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
