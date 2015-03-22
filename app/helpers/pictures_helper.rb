1292b3cb469b7f526fe0cdaf16995d2d1e0be7b5
Edward Byne <ejbyne@gmail.com>
2015-03-22 14:37:30 +0000

    displays when a picture or comment was posted; amends css/formatting

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,10 @@
 module PicturesHelper
+
+  def time_since_created(time_created)
+    return pluralize((((Time.now - time_created) / 3600).round.to_s), 'hour') + " ago" if (Time.now - time_created) < 84600
+    return pluralize((((Time.now - time_created) / 86400).round.to_s), 'day') + " ago" if (Time.now - time_created) < 2678400
+    return pluralize((((Time.now - time_created) / 2678400).round.to_s), 'month') + " ago" if (Time.now - time_created) < 31536000
+    pluralize((((Time.now - time_created) / 31536000).round.to_s), 'year') + " ago"
+  end
+
 end
