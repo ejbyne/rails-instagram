917f4285f90e346a885c52de96079487334bc53f
Edward Byne <ejbyne@gmail.com>
2015-03-23 13:47:34 +0000

    refactors comment feature tests

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -3,77 +3,71 @@ require 'rails_helper'
 feature 'adding comments' do
  
   let!(:user) { User.create(username: "test", email: "test@test.com", password: "testtest") }
-  let!(:beach) { Picture.create(image_file_name: "mock_image", user_id: user.id) }
+  let!(:henry) { Picture.create(image_file_name: "mock_image", user_id: user.id) }
 
   context 'leaving comments' do
 
     scenario 'allows users to leave a comment using a form' do
       sign_up
-      visit '/pictures'
-      find('.picture-link').click
-      fill_in 'Comment', with: 'Awesome'
-      click_button 'Comment'
-      expect(current_path).to eq "/pictures/#{beach.id}"
-      expect(page).to have_content('example: Awesome')
+      leave_comment
+      expect(current_path).to eq("/pictures/#{henry.id}")
+      expect(page).to have_content('ed: Nice picture')
     end
 
     scenario 'does not allow users to leave a comment if they are not logged in' do
-      visit '/pictures'
-      find('.picture-link').click
-      fill_in 'Comment', with: 'Awesome'
-      click_button 'Comment'
+      leave_comment
       expect(page).to have_content('You must be logged in to leave a comment')
-      expect(page).not_to have_content('Awesome')
+      expect(page).not_to have_content('Nice picture')
     end
 
     scenario 'will not let users leave a comment that is too short' do
       sign_up
-      visit '/pictures'
+      visit('/pictures')
       find('.picture-link').click
-      fill_in 'Comment', with: 'Aw'
-      click_button 'Comment'
-      expect('#comment').not_to have_content('Aw')
+      fill_in('Comment', with: '**')
+      click_button('Comment')
+      expect(page).not_to have_selector('#comments', text: '**')
       expect(page).to have_content('error')
     end
 
     scenario 'will not let users leave a comment that is too long' do
       sign_up
-      visit '/pictures'
+      visit('/pictures')
       find('.picture-link').click
-      fill_in 'Comment', with: '*' * 151
-      click_button 'Comment'
-      expect('#comments').not_to have_content('*')
+      fill_in('Comment', with: '*' * 151)
+      click_button('Comment')
+      expect(page).not_to have_selector('#comments', text: '**')
       expect(page).to have_content('error')
     end
 
     scenario 'will let a user delete a comment he or she has created' do
       sign_up
-      visit '/pictures'
-      find('.picture-link').click
-      fill_in 'Comment', with: 'Great'
-      click_button 'Comment'
-      expect(page).to have_content('Great')
-      click_link 'Delete comment'
-      expect(page).not_to have_content('Great')
+      leave_comment
+      expect(page).to have_content('Nice picture')
+      click_link('Delete comment')
+      expect(page).not_to have_content('Nice picture')
     end
 
     scenario 'will not let a user delete a comment he or she has not created' do
-      Comment.create(comment: 'Awful', picture_id: beach.id, user_id: user.id)
+      comment = Comment.create(comment: 'Nice picture', picture_id: henry.id, user_id: user.id)
       sign_up
-      visit '/pictures'
+      visit('/pictures')
       find('.picture-link').click
-      expect(page).to have_content('Awful')
+      expect(page).to have_content('Nice picture')
       expect(page).not_to have_content('Delete comment')
+      page.driver.delete("/pictures/#{henry.id}/comments/#{comment.id}")
+      visit "/pictures/#{henry.id}"
+      expect(page).to have_content('Nice picture')
     end
 
     scenario 'will enable a comment to be added when a picture is posted' do
       sign_up
-      visit '/pictures/new'
-      attach_file('picture[image]', 'spec/features/del.jpg')
-      fill_in 'Comment', with: 'Delboy'
-      click_button 'Upload Picture'
-      expect(page).to have_css("img[src*='del.jpg']")
-      expect(page).to have_content('Delboy')
+      visit('/pictures/new')
+      attach_file('picture[image]', 'spec/features/test_images/henry.jpg')
+      fill_in('Comment', with: 'Nice picture')
+      click_button('Upload Picture')
+      expect(page).to have_css("img[src*='henry.jpg']")
+      expect(page).to have_content('Nice picture')
     end
 
   end
@@ -81,11 +75,18 @@ feature 'adding comments' do
   def sign_up
     visit('/')
     click_link('Sign up')
-    fill_in('Username', with: 'example')
-    fill_in('Email', with: 'test@example.com')
+    fill_in('Username', with: 'ed')
+    fill_in('Email', with: 'ed@test.com')
     fill_in('Password', with: 'testtest')
     fill_in('Password confirmation', with: 'testtest')
     click_button('Sign up')
+  end
+
+  def leave_comment
+    visit('/pictures')
+    find('.picture-link').click
+    fill_in('Comment', with: 'Nice picture')
+    click_button('Comment')
   end
 
 end
