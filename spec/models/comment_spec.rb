a575294470ba794d108f0be2b9fde324f3a55d00
Edward Byne <ejbyne@gmail.com>
2015-03-23 13:15:39 +0000

    refactors model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ describe Comment, :type => :model do
 
   it 'is not valid with a name of less than three characters' do
     user = User.new(username: "test", email: "test@test.com", password: "testtest")
-    comment = Comment.new(comment: 'Aw', user_id: user.id)
+    comment = Comment.new(comment: 'He', user_id: user.id)
     expect(comment).to have(1).error_on(:comment)
     expect(comment).not_to be_valid
   end
@@ -17,7 +17,7 @@ describe Comment, :type => :model do
   end
 
   it 'is not valid if no user id is provided' do
-    comment = Comment.new(comment: '*' * 10)
+    comment = Comment.new(comment: 'Henry')
     expect(comment).to have(1).error_on(:user_id)
     expect(comment).not_to be_valid
   end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Picture, :type => :model do
 
+  it 'is not valid if no image file name is provided' do
+    picture = Picture.new()
+    expect(picture).to have(1).error_on(:image_file_name)
+    expect(picture).not_to be_valid
+  end
+
   it 'is not valid if no user id is provided' do
     picture = Picture.new()
     expect(picture).to have(1).error_on(:user_id)
